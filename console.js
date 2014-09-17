c997afe56c7f200843d55179221eacf76aca16b2
Robert Reinhard <robert.reinhard@bkwld.com>
2014-09-17 16:36:24 -0700

    Never used alert, adding more sub-methods of log

--- console.js
+++ console.js
@@ -3,16 +3,13 @@
 // http://stackoverflow.com/a/5967632/59160
 // --------------------------------------------------
 define(function (require) {
-
-	var fallback_to_alert = false;
 	if (typeof window.console === "undefined") {
-		window.console = {};
-		if (fallback_to_alert) {
-			window.console.log = function(msg) { window.alert(msg); };
-			window.console.debug = function(msg) { window.alert(msg); };
-		} else {
-			window.console.log = function() {};
-			window.console.debug = function() {};
-		}
+		window.console = {
+			log : function() {},
+			debug :function() {},
+			error : function() {},
+			warn : function() {},
+			info : function() {}
+		};
 	}
 });
