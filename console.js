ef4be566cbf065475e223d5e85d1252c3e5f60cc
Robert Reinhard <robert.reinhard@bkwld.com>
2014-09-24 11:19:34 -0700

    Fixing console polyfill when a browser has some console methods but not all

--- console.js
+++ console.js
@@ -3,13 +3,11 @@
 // http://stackoverflow.com/a/5967632/59160
 // --------------------------------------------------
 define(function (require) {
-	if (typeof window.console === "undefined") {
-		window.console = {
-			log : function() {},
-			debug :function() {},
-			error : function() {},
-			warn : function() {},
-			info : function() {}
-		};
+	if (typeof window.console === 'undefined') window.console = {};
+	var methods = ['log', 'debug', 'error', 'warn', 'info'];
+	for (var i=0; i<methods.length; i++) {
+		if (typeof window.console[methods[i]] === 'undefined') {
+			window.console[methods[i]] = function() {};
+		}
 	}
 });
