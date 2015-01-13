93a236bb8e341fb070a20d7957e1cf5809b60a5a
Robert Reinhard <robert.reinhard@bkwld.com>
2015-01-13 13:19:17 -0800

    Adding onClickOutside plugin

---
+++ jquery-on-click-outside.js
@@ -0,0 +1,31 @@
+/**
+ * Listen for a click outside, fire callback, and then remove yoself
+ *
+ * Ex: this.$el.onClickOutside(this.close);
+ */
+define(function (require) {
+	
+	// Dependencies
+	var $ = require('jquery')
+		, $document = $(document)
+	;
+
+	// Plugin definition
+	$.fn.onClickOutside = function (callback) {
+		var $el = $(this);
+
+		// Wait a tick so that the same click that called this doesn't count as
+		// a click outside.
+		setTimeout(function() {
+
+			// Add listener
+			$document.on('click', function(e) {
+				if ($el.is(e.target) || $el.has(e.target).length) return;
+
+				// Remove listener and invoke callback
+				$document.off('click', arguments.callee);
+				callback();
+			});
+		}, 1);
+	};
+});
