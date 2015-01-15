a70737932783b3d98d2f1457295ed78bfa125606
Robert Reinhard <robert.reinhard@bkwld.com>
2015-01-15 14:40:48 -0800

    Adding method to unsubscribe manually to clicks outside

--- jquery-on-click-outside.js
+++ jquery-on-click-outside.js
@@ -9,24 +9,39 @@ define(function (require) {
 	// Dependencies
 	var $ = require('jquery')
 		, $document = $(document)
+		, key = 'on-click-outside-handler'
 	;
 
-	// Plugin definition
+	// Add listner
 	$.fn.onClickOutside = function (callback) {
 		var $el = $(this);
+
+		// Create the handler
+		var handler = function(e) {
+			if ($el.is(e.target) || $el.has(e.target).length !== 0) return;
+			$el.offClickOutside();
+			callback();
+		};
+
+		// Save a reference to the handler on the element so it can be cleared
+		$el.data(key, handler);
 
 		// Wait a tick so that the same click that called this doesn't count as
 		// a click outside.
 		setTimeout(function() {
-
-			// Add listener
-			$document.on('click', function(e) {
-				if ($el.is(e.target) || $el.has(e.target).length !== 0) return;
-
-				// Remove listener and invoke callback
-				$document.off('click', arguments.callee);
-				callback();
-			});
+			$document.on('click', handler);
 		}, 1);
 	};
+
+	// Remove listener
+	$.fn.offClickOutside = function() {
+		var $el = $(this),
+			handler = $el.data(key);
+
+		// If a handler was set, clear it
+		if (handler) {
+			$document.off('click', handler);
+			$el.data(null);
+		}
+	}
 });
