db5b8cb09cdf7a0e8af0e922aa2d16061f254fb5
Eric Howard <howard.eric.m@gmail.com>
2015-05-13 13:52:10 -0700

    Created module to open external and files with _blank

---
+++ external-links.js
@@ -0,0 +1,53 @@
+/**
+ * Any external links or links with file extensions will 
+ * open in new window on click. 
+ */
+define(function (require) {
+
+	var _ = require('lodash')
+	,	host = location.host
+	,	pattern = /^(https?:\/\/[^\/]+)?\/.+\.\w{3,4}$/mg;
+
+	/**
+	 * Handler for external links or files on click
+	 * @param  Event e Click event object
+	 */
+	onExternalLinkClick = function(e) { 
+		e.preventDefault();
+
+		// open in new window
+		window.open($(e.currentTarget).attr('href'), '_blank');
+	};
+
+	// Loop through every link on the page
+	_.each($('a'), function(el,i) {
+		var $a = $(el);
+		var href = $a.attr('href');
+
+		// Check for internal links
+		if( 
+			// undefined HREF 
+			(typeof(href) == 'undefined' ||
+
+			// on this domain
+			href.match(host) || 
+			
+			// nothing
+			href == '' ||	
+			
+			// relative anchor	 
+			href[0] == '#' ||
+			
+			// absolute URI path
+			href[0] == '/') &&
+
+			// AND check for a match for any file extensions
+			!pattern.test(href)
+
+		) return;
+
+		//register handler
+		$a.on('click', onExternalLinkClick);
+	});
+
+});
