82d1a501a9a6d4047c71e1c233e8d9f42fd82153
Robert Reinhard <robert.reinhard@bkwld.com>
2016-01-09 03:26:39 -0800

    Going back to using ajaxPrefilter
    The ajaxSetup() call wasn't affecting Backbone sync.

--- csrf.js
+++ csrf.js
@@ -4,17 +4,17 @@
 // --------------------------------------------------
 define(function (require) {
 	var $ = require('jquery');
-	
+
 	// Get the CSRF token from the meta tags
 	var csrf = $('meta[name="csrf-token"]').attr('content') // Laravel 5
 		|| $('meta[name="csrf"]').attr('content'); // Laravel 4
 	if (!csrf) return;
-	
+
 	// Apply to all AJAX requests
-	$.ajaxSetup({
-		headers: {
-			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'), // Laravel 5
-			'X-CSRF': $('meta[name="csrf-token"]').attr('content') // Laravel 4
-		}
+	$.ajaxPrefilter(function(options) {
+		var headers = options.headers || {};
+		headers['x-csrf-token'] = csrf; // Laravel 5
+		headers['x-csrf'] = csrf; // Laravel 4
+		options.headers = headers;
 	});
 });
